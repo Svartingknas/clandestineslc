59274af6e3399f79882aad78a48c109f52ca785b
Svartingknas <svartingknas@gmail.com>
2016-12-11 21:52:38 -0700

    feat(modal)add email modal

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,52 @@
-// import React from 'react'
-// import { Button, Modal } from 'react-bootstrap';
-// import { ReactDOM } from 'react-dom'
-// import App from './App'
-//
-// const modalInstance = (
-//   <div className="static-modal">
-//     <Modal.Dialog>
-//       <Modal.Header>
-//         <Modal.Title>Modal title</Modal.Title>
-//       </Modal.Header>
-//
-//       <Modal.Body>
-//         One fine body...
-//       </Modal.Body>
-//
-//       <Modal.Footer>
-//         <Button>Close</Button>
-//         <Button bsStyle="primary">Save changes</Button>
-//       </Modal.Footer>
-//
-//     </Modal.Dialog>
-//   </div>
-// );
-//
-// ReactDOM.render(modalInstance, App);
+import React from 'react'
+import { Button, Modal } from 'react-bootstrap'
+import { ReactDOM } from 'react-dom'
+import App from './App'
+import $ from 'jquery'
+import modal, {closeStyle} from 'simple-react-modal'
+
+export default class Contact extends React.Component{
+
+  constructor(){
+    super()
+    this.state = {}
+  }
+
+  show(){
+    this.setState({show: true})
+  }
+
+  close(){
+    this.setState({show: false})
+  }
+
+
+  render(){
+    return (
+      <div>
+        <a onClick={this.show.bind(this)}>Open Modal</a>
+        <Modal
+        className="modal-container"
+        containerStyle={{background: 'blue'}}
+        containerClassName="\"
+        closeOnOuterClick={true}
+        show={this.state.show}
+        onClose={this.close.bind(this)}>
+
+      <a style={closeStyle} onClick={this.close.bind(this)}>X</a>
+
+      <div className="inside-modal">
+        <h4>Communicate with us via E-mail</h4>
+          <form action="mailto:someone@example.com" method="post" enctype="text/plain"><br/>Name:<br/>
+            <input type="text" name="name" /> <br/>E-mail:<br/>
+            <input type="text" name="mail" /> <br/>Comment:<br/>
+            <input type="text" name="comment" size="50" /><br/>
+            <input type="submit" value="Send" />
+            <input type="reset" value="Reset" />
+          </form>
+      </div>
+      </Modal>
+      </div>
+    )
+  }
+}
