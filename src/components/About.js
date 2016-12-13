ca79b60504c0219705794a5db891939eee1c846e
Svartingknas <svartingknas@gmail.com>
2016-12-12 17:51:15 -0700

    feat(style)adjust well

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import NavLink from './NavLink'
 
 export default React.createClass({
   render() {
-    const wellStyles = {maxWidth: 200, margin: '0 auto 10px'};
+    const wellStyles = {maxWidth: 200, margin: '0 auto 10px' };
     return (
       <div>
         <h1>About Us</h1>
